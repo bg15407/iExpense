81d565f3f84c4638de91573a65b7b480611e0a23
Gayan Kalinga <bg15407@gmail.com>
2021-05-23 17:32:17 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gayan Kalinga on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
