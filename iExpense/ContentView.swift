60b19aa916914fdd7a40cec6b79bbf1f8c3d83b4
Gayan Kalinga <bg15407@gmail.com>
2021-05-26 21:17:43 +0530

    Delete rows in a list using on delete modifier with for each
    Delete rows with onDelete(perform: function) attached to the ForEach in a list.
    wirte the function in perform accepting IndexSet type and use array.remove(atOffsets: IndexSet) to remove items from the collections
    Add edit button to the leading navigation bar items to delete multiple items at once

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,40 +7,71 @@
 
 import SwiftUI
 
-struct SecondView: View{
-    @Environment(\.presentationMode) var presentationMode
-    var name: String
-    
+
+struct ContentView: View{
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View{
-        VStack{
-            Text("Hello, \(name) you are now in the second View")
-            Button("dismiss"){
-                presentationMode.wrappedValue.dismiss()
+        NavigationView {
+            VStack{
+                List{
+                    ForEach(numbers, id: \.self){
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeItem)
+                }
+                
+                Button("Add Number"){
+                    numbers.append(self.currentNumber)
+                    currentNumber += 1
+                }
             }
+            .navigationBarItems(leading: EditButton())
         }
-        
+    }
+    
+    func removeItem(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
     }
 }
 
 
-struct ContentView: View {
-    @State private var showSheet = false
-   
-    var body: some View {
-        VStack{
-            Text("I m the First View")
-            
-            Button("Show Second View"){
-                showSheet.toggle()
-            }
-            .sheet(isPresented: $showSheet){
-                SecondView(name: "Gayan")
-            }
-        }
-
-    }
-}
+//Using @Environment to access presentation mode & dismiss()
+//struct SecondView: View{
+//    @Environment(\.presentationMode) var presentationMode
+//    var name: String
+//
+//
+//    var body: some View{
+//        VStack{
+//            Text("Hello, \(name) you are now in the second View")
+//            Button("dismiss"){
+//                presentationMode.wrappedValue.dismiss()
+//            }
+//        }
+//
+//    }
+//}
+//
+//
+//struct ContentView: View {
+//    @State private var showSheet = false
+//
+//    var body: some View {
+//        VStack{
+//            Text("I m the First View")
+//
+//            Button("Show Second View"){
+//                showSheet.toggle()
+//            }
+//            .sheet(isPresented: $showSheet){
+//                SecondView(name: "Gayan")
+//            }
+//        }
+//
+//    }
+//}
 
 
 //Classes Sharing Data
