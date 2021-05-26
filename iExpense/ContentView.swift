0a7dc662f3292959e723f3c08d08ea1b2728ee6c
Gayan Kalinga <bg15407@gmail.com>
2021-05-26 20:29:04 +0530

    Showing and dismisisng a Sheet view
    showing second view as a sheet by changing a state
    accessing the state value from second view  using  environment property wrapper and use dismiss()

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,23 +7,61 @@
 
 import SwiftUI
 
-class User: ObservableObject{
-    @Published var firstName = "Gayan"
-    @Published var lastName = "Kalinga"
-}
-
-struct ContentView: View {
-    @ObservedObject var user = User()
+struct SecondView: View{
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
     
-    var body: some View {
+    
+    var body: some View{
         VStack{
-            Text("Your Name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First Name ", text: $user.firstName)
-            TextField("Last Name", text: $user.lastName)
+            Text("Hello, \(name) you are now in the second View")
+            Button("dismiss"){
+                presentationMode.wrappedValue.dismiss()
+            }
         }
+        
     }
 }
+
+
+struct ContentView: View {
+    @State private var showSheet = false
+   
+    var body: some View {
+        VStack{
+            Text("I m the First View")
+            
+            Button("Show Second View"){
+                showSheet.toggle()
+            }
+            .sheet(isPresented: $showSheet){
+                SecondView(name: "Gayan")
+            }
+        }
+
+    }
+}
+
+
+//Classes Sharing Data
+
+//class User: ObservableObject{
+//    @Published var firstName = "Gayan"
+//    @Published var lastName = "Kalinga"
+//}
+//
+//struct ContentView: View {
+//    @ObservedObject var user = User()
+//
+//    var body: some View {
+//        VStack{
+//            Text("Your Name is \(user.firstName) \(user.lastName)")
+//
+//            TextField("First Name ", text: $user.firstName)
+//            TextField("Last Name", text: $user.lastName)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
